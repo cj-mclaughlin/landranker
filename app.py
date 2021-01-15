5620ed1fb75dc66a27c54f0e3b1f1d329494d622
cj-mclaughlin <cjmclaughlin@wpi.edu>
2021-01-15 01:40:59 +0000

    flask debugging

---
+++ app.py
@@ -0,0 +1,7 @@
+from routes import app
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+
+#def app(environ, start_response):
+#    app.run()
--- routes.py
+++ routes.py
@@ -13,6 +13,14 @@ def landranker():
     Homepage of Landranker Site.
     Offer user ability to select land from random sample
     """
-    land1, land2 = sample_random_lands()
-    webpage = template.render(A=land1["s3_url"], B=land2["s3_url"])
-    return webpage
+    return "Hello World"
+#    land1, land2 = sample_random_lands()
+#    webpage = template.render(A=land1["s3_url"], B=land2["s3_url"])
+#    return webpage
+
+@app.after_request
+def add_header(response):
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = "0"
+    return response
